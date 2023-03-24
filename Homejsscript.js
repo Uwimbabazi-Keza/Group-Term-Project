032a26f850674c349b3f8da59c2a9ce61f7850f0
Uwimbabazi <u.keza@alustudent.com>
2023-03-24 16:06:57 +0200

    make login form functional,connect Home & EHub pgs

--- Homejsscript.js
+++ Homejsscript.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", ()=> {
+document.addEventListener("DOMContentLoaded", ()=> { //Function that toggles between log in and sign up forms
     const loginForm = document.querySelector("#log-in-account");
     const signupForm = document.querySelector("#sign-up-account");
     
     document.querySelector("#sign-up-form-link").addEventListener("click", e => {
-        e.preventDefault();
-        signupForm.classList.remove("form-hidden");
+        e.preventDefault(); //display sign up form
+        signupForm.classList.remove("form-hidden"); //
         loginForm.classList.add("form-hidden");
     });
 
     document.querySelector("#linkbacktologin").addEventListener("click", e => {
-        e.preventDefault();
+        e.preventDefault(); //returns to log in form
         signupForm.classList.add("form-hidden");
         loginForm.classList.remove("form-hidden");
     });
